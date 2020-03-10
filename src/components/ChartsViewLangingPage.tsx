05fbff04fce701b0185fddc4c92f5902a69a4398
yifanwu <yifan1030@gmail.com>
2020-03-10 10:41:20 -0700

    fixed wording and styling issue in landing page

--- src/components/ChartsViewLangingPage.tsx
+++ src/components/ChartsViewLangingPage.tsx
@@ -4,17 +4,25 @@ export const ChartsViewLandingPage: React.StatelessComponent<{}> = () => {
   return <>
     <div className="card landing-card">
       <div>
-        <b><b>charts</b> will be shown here.</b>
-        <br></br>
+        <h4>Load Data</h4>
+        <p>To load data, use <span className="code">m.from_file("path/to/your_data.csv")</span>, the columns will show up to the right.</p>
+        <h4>Load Charts</h4>
+        <p>For any dataframes, you can visualize it here with <span className="code">.vis()</span>.</p>
+        <h4>Making Interactions</h4>
         <p>
-          You can <b>zoom</b> by <span className="action-instruction">scrolling</span> your mouse.
-          You can also <b>select</b> a subset of the data by <span className="action-instruction">shift click</span>, then <span className="action-instruction">drag</span> to draw the brush.
+          All the loaded charts by default are augmented with interactivity. You can <b>zoom</b> by <span className="action-instruction">scrolling</span> your mouse, <b>pan</b> by dragging the view.
+          You can also <b>select</b> a subset of the data by <b><span className="action-instruction">shift click</span></b> with a bar chart, or <span className="action-instruction">shift-drag</span> to draw the brush, with a scatter plot or line chart.
         </p>
+        <h4>Recording/Restoring Interactions</h4>
         <p>
-          You can also save the selections you've made to the left. When you click on a selection, you can go back and restore the state.
+          By default, your interactions are executed via a "log" in a code cell to the left.  You can look at the code to get a sense of what you have interacted with.  You can also execute the code by uncommenting the relevant selections.
         </p>
+        <h4>Toggle and Resize Pannels</h4>
+        <p>
+          To toggle this pannel (restoring the traditional notebook view), click on <b>Toggle Midas</b>, from the menu bar on the top. Similarly, you can click <b>Toggle Column Shelf</b> to just hide the pannel to the right.
+        </p>
+        <p>To resize, you can also drag the left edge of the the main (blue)pannel---the resizer will be highlighted with a darker shade of blue when you hover over.</p>
         <br></br>
-        <p>To load data, use <span className="code">m.read_table("your_data.csv")</span>, the columns will show up in the pannel to the left.  To quickly investigate the interesting columes, click on the column name to generate the <b>code</b> and the <b>charts</b>. For columns of little value, click the cross icon to hide the column.</p>
       </div>
     </div>
   </>;
--- src/components/ProfileShelfLandingPage.tsx
+++ src/components/ProfileShelfLandingPage.tsx
@@ -3,7 +3,10 @@ import React from "react";
 export const ProfileShelfLandingPage: React.StatelessComponent<{}> = () => {
   return <div>
     <div className="card landing-card">
-      <p><b>Columns</b> of loaded data will show here. You can <b>click on the columns</b> to get the distribution of the data. You can also <b>drag</b> this window around to avoid </p>
+      <h4>Column Shelf</h4>
+      <p><b>Columns</b> of loaded data will show here. You can click on the x symbol to the right to hide the column from view.</p>
+      <h4>See Distributions</h4>
+      <p>You can <b>click on the column</b> to get the distribution of the data.</p>
     </div>
   </div>;
 };
