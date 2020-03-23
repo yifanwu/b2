93419e533b47b3bf6d2b065c607a12462af5e28e
yifanwu <yifan1030@gmail.com>
2020-03-23 16:01:46 -0700

    added a loading and catching util fun from ds100

--- midas/util/utils.py
+++ midas/util/utils.py
@@ -8,6 +8,9 @@ import sqlite3
 from datetime import datetime
 from shutil import copyfile, copy
 from re import sub
+import requests
+from pathlib import Path
+import time 
 
 from typing import Tuple, List, Optional
 from IPython import get_ipython  # type: ignore
@@ -17,6 +20,38 @@ from midas.constants import ISDEBUG
 from midas.midas_algebra.selection import ColumnRef, EmptySelection, SelectionValue
 from midas.util.errors import UserError, InternalLogicalError
 
+
+def fetch_and_cache(data_url, file, data_dir="data", force=False):
+    """
+    Download and cache a url and return the file object.
+    
+    data_url: the web address to download
+    file: the file in which to save the results.
+    data_dir: (default="data") the location to save the data
+    force: if true the file is always re-downloaded 
+    
+    return: The pathlib.Path object representing the file.
+    """
+    
+    data_dir = Path(data_dir)
+    data_dir.mkdir(exist_ok = True)
+    file_path = data_dir / Path(file)
+    # If the file already exists and we want to force a download then
+    # delete the file first so that the creation date is correct.
+    if force and file_path.exists():
+        file_path.unlink()
+    if force or not file_path.exists():
+        print('Downloading...', end=' ')
+        resp = requests.get(data_url)
+        with file_path.open('wb') as f:
+            f.write(resp.content)
+        print('Done!')
+        last_modified_time = time.ctime(file_path.stat().st_mtime)
+    else:
+        last_modified_time = time.ctime(file_path.stat().st_mtime)
+        print("Using cached version that was downloaded (UTC):", last_modified_time)
+    return file_path
+    
 
 def isnotebook():
     try:
